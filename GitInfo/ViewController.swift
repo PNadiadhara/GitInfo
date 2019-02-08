3f4fc7d9f317a2dea0ac6795eab870cfc8a5a1d9
pnadiadhara <priteshrna@gmail.com>
2019-02-08 10:54:58 -0500

    First Commit

---
+++ GitInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitInfo
+//
+//  Created by Pritesh Nadiadhara on 2/8/19.
+//  Copyright © 2019 Pritesh Nadiadhara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("working on git")
+    }
+
+
+}
+
